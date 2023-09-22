a3cec17ed024cc6c6b75be7d12777b68acc43dce
Ethkopp <142931620+Ethkopp@users.noreply.github.com>
2023-09-22 10:15:09 -0800

    completes assignment

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,22 +1,42 @@
 extends CharacterBody2D
 
 var speed = 5
-var max_speed = 100
+var max_speed = 1000
 var rotate_speed = 0.08
+var nose = Vector2(0,-60)
+var Bullet = load("res://Assets/Bullet.png")
 
-
-func _physics_process(delta):
+func get_input():
+	var to_return = Vector2.ZERO
+	$Exhaust.hide()
+	if Input.is_action_pressed("Forward"):
+		to_return += Vector2(0,-1)
+		$Exhaust.show()
 	if Input.is_action_pressed("Left"):
 		rotation -= rotate_speed
 	if Input.is_action_pressed("Right"):
 		rotation += rotate_speed
-	if Input.is_action_pressed("Forward"):
-		velocity = velocity + Vector2(0,-speed).rotated(rotation)
+	return to_return.rotated(rotation)
+
+
+func _physics_process(delta):
+	velocity += get_input()*speed
+	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
+
+	position.x = wrapf(position.x, 0, 1152)
+	position.y = wrapf(position.y, 0, 648)
+	velocity = velocity.normalized() * clamp(velocity.length(),0, max_speed)
 		
-		position.x = wrapf(position.x, 0, 1152)
-		position.y = wrapf(position.x, 0, 648)
-		velocity = velocity.normalized() = clamp(velocity.length(), 0, max_speed)
+
 		
 		
-		move_and_slide()
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("Shoot"):
+		var bullet = Bullet.instantiate()
+		bullet.postition = position + nose.rotated(rotation)
+		bullet.rotation = rotation
+		var Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			Effects.add_child(bullet) 
 	
