4f8e4b83b8ffaf09719863576b843b965109a1a4
Ethkopp <142931620+Ethkopp@users.noreply.github.com>
2023-09-25 10:25:36 -0800

    Completes Assignment

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,7 +4,7 @@ var speed = 5
 var max_speed = 1000
 var rotate_speed = 0.08
 var nose = Vector2(0,-60)
-var Bullet = load("res://Assets/Bullet.png")
+var Bullet = load("res://Player/bullet.tscn")
 
 func get_input():
 	var to_return = Vector2.ZERO
@@ -19,7 +19,7 @@ func get_input():
 	return to_return.rotated(rotation)
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	velocity += get_input()*speed
 	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
 
@@ -27,14 +27,13 @@ func _physics_process(delta):
 	position.y = wrapf(position.y, 0, 648)
 	velocity = velocity.normalized() * clamp(velocity.length(),0, max_speed)
 		
-
 		
 		
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("Shoot"):
 		var bullet = Bullet.instantiate()
-		bullet.postition = position + nose.rotated(rotation)
+		bullet.position = position + nose.rotated(rotation)
 		bullet.rotation = rotation
 		var Effects = get_node_or_null("/root/Game/Effects")
 		if Effects != null:
